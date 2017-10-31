d8257a887c50ca77d5d933a003cf04a4504a14c3
alekseik1 <alekseik1@yandex.ru>
2017-10-31 20:11:36 +0300

    Исправил ошибки, ломавшие компиляцию

--- lection 9/Student_2.cpp
+++ lection 9/Student_2.cpp
@@ -29,9 +29,9 @@ int main()
     c.aging();
     c.print();
 
-    //ps = new Student;   // Won't work; we don't have empty constuctor!
-    ps = new Student(18, "Misha");  // What is OK
+    //Student ps = new Student;   // Won't work; we don't have empty constuctor!
+    Student* ps = new Student(18, "Misha");  // What is OK
 
-    //m = new Student[10]; // Won't work; no default constuctor!
+    //Student m = new Student[10]; // Won't work; no default constuctor!
 
 }
